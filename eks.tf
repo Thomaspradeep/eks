a7c7717607714bd5315bc9dc8c01abf03cc4eb5b
pradeep <thomaspradeep06@gmail.com>
2024-09-19 19:01:10 +0530

    updated

--- eks.tf
+++ eks.tf
@@ -1,11 +1,8 @@
 resource "aws_eks_cluster" "mycluster" {
     name = "mycluster"
-    role_arn = var.eks_role
+    role_arn = "arn:aws:iam::650251688303:role/AmazonEKSRole"
 
     vpc_config = {
         subnet_ids = ["subnet-0bde0e1c7eae6887c","subnet-0903f761c530aff48"]
     }
-    depends_on = [
-        "arn:aws:iam::650251688303:role/AmazonEKSRole"
-     ]
 }
